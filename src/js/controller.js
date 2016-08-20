0f242e160979bd39cfae8fcc43815434a0139fb7
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2016-08-20 18:40:56 +0200

    Show/hide password Hash show

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,17 +1,10 @@
-let Hashes = require('jshashes');
-let md5 = new Hashes.MD5();
-
 module.exports = function (action, input, secret, output) {
     let update = function () {
         var text = input.value;
         var key = secret.value;
-        if (text && key) {
-            output.value = action(text, md5.hex(key));
-        } else {
-            output.value = '';
-        }
+        output.value = action(text, key);
     };
 
     input.oninput = update;
-    secret.oninput = update;
+    secret.onchange = update;
 };
--- src/js/ui/encrypt.js
+++ src/js/ui/encrypt.js
@@ -1,15 +1,42 @@
 var vernam = require('./../cipher/vernam')({});
 var controller = require('./../controller');
+let Hashes = require('jshashes');
+let md5 = new Hashes.MD5();
 
-var cipherTextInput = document.getElementById("encrypt-cipher");
-controller(vernam.encrypt,
-    document.getElementById("encrypt-text"),
-    document.getElementById("encrypt-secret"),
-    cipherTextInput);
+var text = document.getElementById("encrypt-text");
+var secret = document.getElementById("encrypt-secret");
+var secretHash = document.getElementById("encrypt-secret-hash");
+var cipherText = document.getElementById("encrypt-cipher");
 
-document.getElementById('copy-to-clipboard').onclick = function (e) {
-    cipherTextInput.select();
-    var successful = document.execCommand('copy');
+var update = function () {
+    var secretValue = secret.value;
+    var hash;
+    if (secretValue) {
+        hash = md5.hex(secretValue);
+        secretHash.value = hash.substr(0, text.value.length);
+    }
+    cipherText.value = vernam.encrypt(text.value, hash);
+};
+
+text.oninput = update;
+secret.oninput = update;
+
+
+document.getElementById('show-hide-button').onclick = function () {
+    var type = secret.type.toLowerCase();
+    console.log(`Secret type: ${type}`);
+    secret.type = type === 'password' ? 'text' : 'password';
+    return false;
+};
+
+document.getElementById('copy-to-clipboard-button').onclick = function (e) {
+    cipherText.select();
+    var success = document.execCommand('copy');
+    if (success) {
+        console.log(`\'${cipherText.value}\' copied to clipboard!`);
+    } else {
+        console.error('Failed to copy to clipboard!')
+    }
     e.preventDefault();
     return false;
 };
