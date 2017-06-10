13fe1a20c8cf10d185ebc0c9e6afd24c4cdf6bc0
GeorgeGorbanev <GeorgeGorbanev@gmail.com>
2017-06-10 16:50:34 +0300

    url striper eslint spaces fix

--- src/js/url/stripper.js
+++ src/js/url/stripper.js
@@ -11,14 +11,14 @@ const URL_REGEXP = new RegExp([
 const parse = (url) => {
   const match = url.match(URL_REGEXP);
   return match && {
-        protocol: match[2],
-        host: match[3],
-        hostname: match[4],
-        port: match[5],
-        pathname: match[6],
-        search: match[7],
-        hash: match[8]
-      };
+    protocol: match[2],
+    host: match[3],
+    hostname: match[4],
+    port: match[5],
+    pathname: match[6],
+    search: match[7],
+    hash: match[8]
+  };
 };
 
 export default (url, excludeWWW = true) => {
