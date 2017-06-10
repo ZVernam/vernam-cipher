ceeb3902d60017745e7197254a3b08fc1acb59b0
GeorgeGorbanev <GeorgeGorbanev@gmail.com>
2017-06-10 17:21:48 +0300

    chipher summary + score

--- src/js/summary.js
+++ src/js/summary.js
@@ -4,5 +4,6 @@ export default {
     document.getElementById(`guesses`).innerHTML = newSummary.guesses;
     document.getElementById(`guesses-log`).innerHTML = newSummary.guesses_log10;
     document.getElementById(`calctime`).innerHTML = newSummary.calc_time;
+    document.getElementById(`score`).innerHTML = newSummary.score;
   }
 };
