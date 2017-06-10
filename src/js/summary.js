7352b8e6425750e6b9359b973aeac499074bce3a
GeorgeGorbanev <GeorgeGorbanev@gmail.com>
2017-06-10 19:27:31 +0300

    summary calculate nodes only once

--- src/js/summary.js
+++ src/js/summary.js
@@ -1,9 +1,15 @@
+let passwordLengthNode = document.getElementById(`password-length`);
+let guessesNode = document.getElementById(`guesses`);
+let guessesLogNode = document.getElementById(`guesses-log`);
+let calcTimeNode = document.getElementById(`calctime`);
+let scoreNode = document.getElementById(`score`);
+
 export default {
   update(newSummary) {
-    document.getElementById(`password-length`).innerHTML = newSummary.password.length;
-    document.getElementById(`guesses`).innerHTML = newSummary.guesses;
-    document.getElementById(`guesses-log`).innerHTML = newSummary.guesses_log10;
-    document.getElementById(`calctime`).innerHTML = newSummary.calc_time;
-    document.getElementById(`score`).innerHTML = newSummary.score;
+    passwordLengthNode.innerHTML = newSummary.password.length;
+    guessesNode.innerHTML = newSummary.guesses;
+    guessesLogNode.innerHTML = newSummary.guesses_log10;
+    calcTimeNode.innerHTML = newSummary.calc_time;
+    scoreNode.innerHTML = newSummary.score;
   }
 };
