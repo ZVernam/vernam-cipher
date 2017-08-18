d51c9c4116dd529751191f87079f50e8978562f1
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2017-08-18 23:13:39 +0200

    show/hide summary

--- src/js/summary.js
+++ src/js/summary.js
@@ -1,15 +1,28 @@
+const COLLAPSED_CLASS_NAME = `collapsed`;
+
 const passwordLengthNode = document.getElementById(`password-length`);
 const guessesNode = document.getElementById(`guesses`);
 const guessesLogNode = document.getElementById(`guesses-log`);
 const calcTimeNode = document.getElementById(`calctime`);
 const scoreNode = document.getElementById(`score`);
+const expandCollapseContentNode = document.getElementById(`expand-collapse-content`);
+
+let collapsed = expandCollapseContentNode.classList.contains(COLLAPSED_CLASS_NAME);
+document.getElementById(`expand-collapse-button`).onclick = function () {
+  collapsed = !collapsed;
+
+  const useElement = this.querySelector(`use`);
+  const icon = `open-iconic.svg#caret-${collapsed ? `right` : `bottom`}`;
+  useElement.setAttribute(`xlink:href`, icon);
+  expandCollapseContentNode.classList.toggle(COLLAPSED_CLASS_NAME);
+};
 
 export default {
   update(newSummary) {
-    passwordLengthNode.innerHTML = newSummary.password.length;
-    guessesNode.innerHTML = newSummary.guesses;
-    guessesLogNode.innerHTML = newSummary.guesses_log10;
-    calcTimeNode.innerHTML = newSummary.calc_time;
-    scoreNode.innerHTML = newSummary.score;
+    passwordLengthNode.textContent = newSummary.password.length;
+    guessesNode.textContent = newSummary.guesses;
+    guessesLogNode.textContent = newSummary.guesses_log10;
+    calcTimeNode.textContent = newSummary.calc_time;
+    scoreNode.textContent = newSummary.score;
   }
 };
