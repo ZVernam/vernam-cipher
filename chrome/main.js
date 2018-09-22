f99efdac4c28bc2c40f524411f980d22df264cd2
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2018-09-23 02:55:09 +0300

    add hashed option

--- chrome/main.js
+++ chrome/main.js
@@ -5,14 +5,18 @@ import buildPattern from '../lib/src/pattern/pattern';
 window.onload = () => {
   const text = document.getElementById(`encrypt-text`);
   const secret = document.getElementById(`encrypt-secret`);
+  const hashUnhashButton = document.getElementById(`hash-unhash-button`);
 
   chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
     const url = stripper(tabs[0].url);
-    chrome.storage.sync.get(null, ({pattern, user}) => {
-      if (pattern && user) {
-        text.value = buildPattern(pattern)({site: {url}, user});
+    chrome.storage.sync.get(null, ({pattern, token, hashed}) => {
+      if (pattern && token) {
+        text.value = buildPattern(pattern)({site: {url}, user: {token}});
       } else {
         text.value = url;
+      }
+      if (hashed) {
+        hashUnhashButton.click();
       }
       secret.focus();
     });
--- chrome/options.js
+++ chrome/options.js
@@ -1,44 +1,94 @@
-const patternElement = document.getElementById('pattern');
-const tokenElement = document.getElementById('token');
+const CLEANUP_DELAY = 750;
+
+const mapping = {
+  pattern: {
+    id: 'pattern',
+    defaultValue: '${site.url}${user.token}'
+  },
+  token: {
+    id: 'token',
+    defaultValue: ''
+  },
+  hashed: {
+    id: 'hashed',
+    defaultValue: true
+  }
+};
+
+const storage = chrome.storage.sync;
+const runtime = chrome.runtime;
+
+const defaultValues = {};
+for (const [name, value] of Object.entries(mapping)) {
+  value.element = document.getElementById(value.id);
+  defaultValues[name] = value.defaultValue;
+}
+
+const fillElements = (data) => {
+  for (const [name, value] of Object.entries(data)) {
+    const {element} = mapping[name];
+    switch (typeof value) {
+      case 'boolean':
+        element.checked = value;
+        break;
+      default:
+        element.value = value;
+    }
+  }
+};
+
+const promisify = (fn) => {
+  return (...args) => new Promise((onSuccess, onFail) => {
+    fn(...args, (data) => {
+      if (runtime.lastError) {
+        onFail(runtime.lastError);
+      } else {
+        onSuccess(...(data ? [data] : args));
+      }
+    });
+  });
+};
+
+const handlePromise = (promise, successMessage, failMessage) => promise.
+  then(() => statusElement.textContent = successMessage).
+  catch(({message = failMessage}) => statusElement.textContent = message).
+  then(() => setTimeout(() => statusElement.textContent = '', CLEANUP_DELAY));
+
+
+const myStorage = {
+  save() {
+    const data = {};
+
+    for (const [name, value] of Object.entries(mapping)) {
+      const input = value.element;
+      if (input.type === `checkbox`) {
+        data[name] = input.checked;
+      } else {
+        data[name] = input.value;
+      }
+    }
+
+    return promisify(storage.set)(data);
+  },
+  load() {
+    return promisify(storage.get)(defaultValues).then(fillElements);
+  },
+  reset() {
+    fillElements(defaultValues);
+    return this.save(defaultValues);
+  }
+};
 
 const statusElement = document.getElementById('status');
-const CLEANUP_DELAY = 750;
-const DEFAULT_PATTERN = '${site.url}${user.token}';
-const DEFAULT_TOKEN = '';
-const storage = chrome.storage.sync;
 
 // Saves options to chrome.storage
-const save = () => {
-  storage.set({
-    pattern: patternElement.value,
-    user: {
-      token: tokenElement.value
-    }
-  }, () => {
-    // Update status to let user know options were saved.
-    statusElement.textContent = 'Options saved.';
-    setTimeout(() => statusElement.textContent = '', CLEANUP_DELAY);
-  });
-};
+const save = () => handlePromise(myStorage.save(), 'Options saved.', 'Failed to save options.');
 
-const reset = () => {
-  patternElement.value = DEFAULT_PATTERN;
-  tokenElement.value = DEFAULT_TOKEN;
-  save();
-};
+// Drop options to defaults
+const reset = () => handlePromise(myStorage.reset(), 'Options have been dropped.', 'Failed to reset options.');
 
-// Restores preferences stored in chrome.storage.
-const restore = () => {
-  storage.get({
-    pattern: DEFAULT_PATTERN,
-    user: {
-      token: DEFAULT_TOKEN
-    }
-  }, (data) => {
-    patternElement.value = data.pattern;
-    tokenElement.value = data.user.token;
-  });
-};
+// Loads preferences stored in chrome.storage.
+const restore = () => handlePromise(myStorage.load(), 'Options were loaded successfully.', 'Failed to load options.');
 
 document.addEventListener('DOMContentLoaded', restore);
 document.getElementById('save').addEventListener('click', save);
