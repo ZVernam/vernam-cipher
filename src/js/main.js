51edaab677564f8556cee08b7eb16160726fcf03
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2025-12-11 00:03:51 +0700

    fix main

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,3 @@
 import './controller';
 import './link';
 
-if (!window.Telegram?.WebApp) {
-  // Режим браузера
-  document.body.innerHTML += "<div>Opened outside Telegram</div>";
-} else {
-  // Режим Telegram WebApp
-  const tg = window.Telegram.WebApp;
-  tg.MainButton.show();
-}
-
