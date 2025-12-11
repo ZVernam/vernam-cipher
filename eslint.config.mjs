2c9be802ab35d4eef42adebb29c502a9f4efdc13
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2025-12-11 13:50:48 +0700

    update eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+// eslint.config.js
+
+import {defineConfig} from "eslint/config";
+import js from "@eslint/js";
+
+export default defineConfig([
+  {ignores: ['build/**/*']},
+  {
+    files: ["src/**/*.js"],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        window: true,
+        console: true,
+        document: true,
+        navigator: true,
+        require: true,
+        module: true,
+        describe: true,
+        it: true,
+        before: true,
+        after: true,
+        beforeEach: true,
+        afterEach: true,
+      },
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      'no-console': 'off',
+    },
+  },
+]);
