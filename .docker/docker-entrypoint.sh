2a980fcbf08b86139260375ecba2ff96eca6a856
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2025-12-13 20:12:16 +0700

    fix entrypoint

--- .docker/docker-entrypoint.sh
+++ .docker/docker-entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
 
-VALUE="${SERVER_API-__SERVER_API__}"
+echo "VAR=${SERVER_API}"
 
-ESCAPED_VALUE=$(printf '%s' "$VALUE" | sed 's/[\/&|]/\\&/g')
+ESCAPED_VALUE=$(printf '%s' "$SERVER_API" | sed 's/[\/&|]/\\&/g')
 
 sed -i "s|__SERVER_API__|$ESCAPED_VALUE|g" /usr/share/nginx/html/index.html
 
